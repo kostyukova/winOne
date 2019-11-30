a2ecd70086f32feef4e9c70a9ee3d4996a7787a0
Elena Kostyukova <Elena.Kostyukova@ingrammicro.com>
2019-11-30 13:16:42 +0700

    Spring Boot Hello Application

---
+++ service/src/main/java/com/my/service/SpringBootWinOneApplication.java
@@ -0,0 +1,12 @@
+package com.my.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWinOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWinOneApplication.class, args);
+    }
+}
